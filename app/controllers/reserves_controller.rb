f9b3e993088ad349a16fc3f3e70f03c8e5fc96a2
shikino <sshhiikkiinnoo@gmail.com>
2021-10-29 20:48:23 +0900

    確認画面からデータベースに保存

--- app/controllers/reserves_controller.rb
+++ app/controllers/reserves_controller.rb
@@ -5,6 +5,13 @@ class ReservesController < ApplicationController
   end
 
   def create
+    @reserve = Reserve.new(params.require(:reserve).permit(:check_in, :check_out, :people, :total_price, :hotel_id))
+    if @reserve.save
+      flash[:notice] = "予約をしました"
+      redirect_to :hotels
+    else
+      render "hotels"
+    end
   end
 
   def show
