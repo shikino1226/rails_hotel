c149f27d251e70779345683e0d49db4060605b39
shikino <sshhiikkiinnoo@gmail.com>
2021-11-01 17:33:50 +0900

    ログイン項目追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_31_041947) do
+ActiveRecord::Schema.define(version: 2021_11_01_081911) do
 
   create_table "hotels", force: :cascade do |t|
     t.string "name"
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2021_10_31_041947) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.string "avater"
+    t.text "profile"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
