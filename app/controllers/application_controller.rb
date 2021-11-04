0315e3bd3712627d5f878984e940f3c1f26a8f78
shikino <sshhiikkiinnoo@gmail.com>
2021-11-04 15:53:45 +0900

    プロフィール画像表示(不完全)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller? 
   before_action :set_current_user
   before_action :set_search
+  
 
   def set_search
     #@search = Article.search(params[:q])
@@ -12,7 +13,7 @@ class ApplicationController < ActionController::Base
   private
 
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) 
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email, :avater]) 
     end
 
     def set_current_user
