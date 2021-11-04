68b1dbcd9b51fb558673172c32e4210f42c7754b
shikino <sshhiikkiinnoo@gmail.com>
2021-11-04 17:24:46 +0900

    アバター表示

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :reservations, dependent: :destroy
   has_many :hotels, dependent: :destroy
+  mount_uploader :avatar, AvatarUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
