7edea631ac66fa35289299dd338ce9a97986b094
shikino <sshhiikkiinnoo@gmail.com>
2021-11-08 12:40:46 +0900

    ユーザー編集画面変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,15 @@
 Rails.application.routes.draw do
-
   devise_for :users
   root to: 'hotels#home'
-
   resources :hotels do
     collection do
       get 'search'
     end
   end
-
   get 'home/index' => 'home#index'
   get 'home/index2' => 'home#index2'
   get 'home/profile' => 'home#profile'
   post 'home/profile' => 'home#update'
   resources :home
-
   resources :reservations
 end
